9986cea0b2b0a46d237cb3c6dc67d798b69f345e
azimutt19 <34372894+azimutt19@users.noreply.github.com>
2017-12-11 00:39:54 +0000

    Mastermind

---
+++ Exercicio16/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.btn_proxima = New System.Windows.Forms.Button()
+        Me.btn_sair = New System.Windows.Forms.Button()
+        Me.tb_tentativas = New System.Windows.Forms.TextBox()
+        Me.btn_reiniciar = New System.Windows.Forms.Button()
+        Me.lbl_tentativas = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Amarelo", "Azul", "Laranja", "Rosa", "Roxo", "Verde"})
+        Me.ListBox1.Location = New System.Drawing.Point(50, 78)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.Sorted = True
+        Me.ListBox1.TabIndex = 1
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Items.AddRange(New Object() {"Amarelo", "Azul", "Laranja", "Rosa", "Roxo", "Verde"})
+        Me.ListBox2.Location = New System.Drawing.Point(227, 78)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox2.Sorted = True
+        Me.ListBox2.TabIndex = 2
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Items.AddRange(New Object() {"Amarelo", "Azul", "Laranja", "Rosa", "Roxo", "Verde"})
+        Me.ListBox3.Location = New System.Drawing.Point(399, 78)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox3.Sorted = True
+        Me.ListBox3.TabIndex = 3
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Items.AddRange(New Object() {"Amarelo", "Azul", "Laranja", "Rosa", "Roxo", "Verde"})
+        Me.ListBox4.Location = New System.Drawing.Point(572, 78)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox4.Sorted = True
+        Me.ListBox4.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(50, 245)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(118, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(227, 245)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(118, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(399, 245)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(118, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(572, 245)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(118, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'btn_proxima
+        '
+        Me.btn_proxima.Location = New System.Drawing.Point(62, 363)
+        Me.btn_proxima.Name = "btn_proxima"
+        Me.btn_proxima.Size = New System.Drawing.Size(148, 51)
+        Me.btn_proxima.TabIndex = 10
+        Me.btn_proxima.Text = "&Proxima Tentativa"
+        Me.btn_proxima.UseVisualStyleBackColor = True
+        '
+        'btn_sair
+        '
+        Me.btn_sair.Location = New System.Drawing.Point(371, 477)
+        Me.btn_sair.Name = "btn_sair"
+        Me.btn_sair.Size = New System.Drawing.Size(148, 51)
+        Me.btn_sair.TabIndex = 11
+        Me.btn_sair.Text = "&Sair"
+        Me.btn_sair.UseVisualStyleBackColor = True
+        '
+        'tb_tentativas
+        '
+        Me.tb_tentativas.Location = New System.Drawing.Point(552, 377)
+        Me.tb_tentativas.Name = "tb_tentativas"
+        Me.tb_tentativas.ReadOnly = True
+        Me.tb_tentativas.Size = New System.Drawing.Size(100, 20)
+        Me.tb_tentativas.TabIndex = 12
+        '
+        'btn_reiniciar
+        '
+        Me.btn_reiniciar.Location = New System.Drawing.Point(161, 477)
+        Me.btn_reiniciar.Name = "btn_reiniciar"
+        Me.btn_reiniciar.Size = New System.Drawing.Size(148, 51)
+        Me.btn_reiniciar.TabIndex = 13
+        Me.btn_reiniciar.Text = "&Reiniciar"
+        Me.btn_reiniciar.UseVisualStyleBackColor = True
+        '
+        'lbl_tentativas
+        '
+        Me.lbl_tentativas.AutoSize = True
+        Me.lbl_tentativas.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_tentativas.Location = New System.Drawing.Point(427, 380)
+        Me.lbl_tentativas.Name = "lbl_tentativas"
+        Me.lbl_tentativas.Size = New System.Drawing.Size(78, 17)
+        Me.lbl_tentativas.TabIndex = 14
+        Me.lbl_tentativas.Text = "Tentativas:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(717, 550)
+        Me.Controls.Add(Me.lbl_tentativas)
+        Me.Controls.Add(Me.btn_reiniciar)
+        Me.Controls.Add(Me.tb_tentativas)
+        Me.Controls.Add(Me.btn_sair)
+        Me.Controls.Add(Me.btn_proxima)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Mastermind"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents btn_proxima As Button
+    Friend WithEvents btn_sair As Button
+    Friend WithEvents tb_tentativas As TextBox
+    Friend WithEvents btn_reiniciar As Button
+    Friend WithEvents lbl_tentativas As Label
+End Class
---
+++ Exercicio16/Form1.vb
@@ -0,0 +1,227 @@
+﻿Public Class Form1
+    Dim i = 0, tentativas = 0
+    Dim a, b, c, d, amarelo, azul, laranja, rosa, roxo, verde As Integer
+
+    Dim rand As New Random
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        a = rand.Next(6)
+        b = rand.Next(6)
+        Do While b = a
+            b = rand.Next(6)
+        Loop
+        c = rand.Next(6)
+        Do While c = a Or c = b
+            c = rand.Next(6)
+        Loop
+        d = rand.Next(6)
+        Do While d = a Or d = b Or d = c
+            d = rand.Next(6)
+        Loop
+
+        For i = 0 To 100
+            If ListBox1.SelectedItems.Count = 1 And ListBox2.SelectedItems.Count = 1 And ListBox3.SelectedItems.Count = 1 AndAlso ListBox4.SelectedItems.Count = 1 Then
+                tentativas = tentativas + 1
+                ListBox1.ClearSelected()
+                ListBox2.ClearSelected()
+                ListBox3.ClearSelected()
+                ListBox4.ClearSelected()
+                i = i + 1
+            End If
+        Next
+        tb_tentativas.Text = tentativas
+
+
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedItem Is "Amarelo" And a = 0 Then
+            TextBox1.Text = ListBox1.SelectedItem
+        End If
+        If ListBox1.SelectedItem Is "Azul" And a = 1 Then
+            TextBox1.Text = ListBox1.SelectedItem
+        End If
+        If ListBox1.SelectedItem Is "Laranja" And a = 2 Then
+            TextBox1.Text = ListBox1.SelectedItem
+        End If
+        If ListBox1.SelectedItem Is "Rosa" And a = 3 Then
+            TextBox1.Text = ListBox1.SelectedItem
+        End If
+        If ListBox1.SelectedItem Is "Roxo" And a = 4 Then
+            TextBox1.Text = ListBox1.SelectedItem
+        End If
+        If ListBox1.SelectedItem Is "Verde" And a = 5 Then
+            TextBox1.Text = ListBox1.SelectedItem
+        End If
+        If ListBox1.SelectedItems.Count > 0 Then
+            ListBox1.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextBox7_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub tb_tentativas_TextChanged(sender As Object, e As EventArgs) Handles tb_tentativas.TextChanged
+
+    End Sub
+
+    Private Sub btn_reiniciar_Click(sender As Object, e As EventArgs) Handles btn_reiniciar.Click
+        tb_tentativas.Text = 0
+        ListBox1.ClearSelected()
+        ListBox2.ClearSelected()
+        ListBox3.ClearSelected()
+        ListBox4.ClearSelected()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        ListBox1.Enabled = True
+        ListBox2.Enabled = True
+        ListBox3.Enabled = True
+        ListBox4.Enabled = True
+
+    End Sub
+
+    Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+        If ListBox2.SelectedItem Is "Amarelo" And b = 0 Then
+            TextBox2.Text = ListBox2.SelectedItem
+        End If
+        If ListBox2.SelectedItem Is "Azul" And b = 1 Then
+            TextBox2.Text = ListBox2.SelectedItem
+        End If
+        If ListBox2.SelectedItem Is "Laranja" And b = 2 Then
+            TextBox2.Text = ListBox2.SelectedItem
+        End If
+        If ListBox2.SelectedItem Is "Rosa" And b = 3 Then
+            TextBox2.Text = ListBox2.SelectedItem
+        End If
+        If ListBox2.SelectedItem Is "Roxo" And b = 4 Then
+            TextBox2.Text = ListBox2.SelectedItem
+        End If
+        If ListBox2.SelectedItem Is "Verde" And b = 5 Then
+            TextBox2.Text = ListBox2.SelectedItem
+        End If
+        If ListBox2.SelectedItems.Count > 0 Then
+            ListBox2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ListBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox3.SelectedIndexChanged
+        If ListBox3.SelectedItem Is "Amarelo" And c = 0 Then
+            TextBox3.Text = ListBox3.SelectedItem
+        End If
+        If ListBox3.SelectedItem Is "Azul" And c = 1 Then
+            TextBox3.Text = ListBox3.SelectedItem
+        End If
+        If ListBox3.SelectedItem Is "Laranja" And c = 2 Then
+            TextBox3.Text = ListBox3.SelectedItem
+        End If
+        If ListBox3.SelectedItem Is "Rosa" And c = 3 Then
+            TextBox3.Text = ListBox3.SelectedItem
+        End If
+        If ListBox3.SelectedItem Is "Roxo" And c = 4 Then
+            TextBox3.Text = ListBox3.SelectedItem
+        End If
+        If ListBox3.SelectedItem Is "Verde" And c = 5 Then
+            TextBox3.Text = ListBox3.SelectedItem
+        End If
+        If ListBox3.SelectedItems.Count > 0 Then
+            ListBox3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub ListBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox4.SelectedIndexChanged
+        If ListBox4.SelectedItem Is "Amarelo" And d = 0 Then
+            TextBox4.Text = ListBox4.SelectedItem
+        End If
+        If ListBox4.SelectedItem Is "Azul" And d = 1 Then
+            TextBox4.Text = ListBox4.SelectedItem
+        End If
+        If ListBox4.SelectedItem Is "Laranja" And d = 2 Then
+            TextBox4.Text = ListBox4.SelectedItem
+        End If
+        If ListBox4.SelectedItem Is "Rosa" And d = 3 Then
+            TextBox4.Text = ListBox4.SelectedItem
+        End If
+        If ListBox4.SelectedItem Is "Roxo" And d = 4 Then
+            TextBox4.Text = ListBox4.SelectedItem
+        End If
+        If ListBox4.SelectedItem Is "Verde" And d = 5 Then
+            TextBox4.Text = ListBox4.SelectedItem
+        End If
+        If ListBox4.SelectedItems.Count > 0 Then
+            ListBox4.Enabled = False
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        If TextBox1.Text IsNot "" And TextBox2.Text IsNot "" And TextBox3.Text IsNot "" AndAlso TextBox4.Text IsNot "" Then
+            MsgBox("Parabéns, ganhou!!")
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If TextBox1.Text IsNot "" And TextBox2.Text IsNot "" And TextBox3.Text IsNot "" AndAlso TextBox4.Text IsNot "" Then
+            MsgBox("Parabéns, ganhou!! ")
+        End If
+    End Sub
+    Private Sub btn_limpar_Click(sender As Object, e As EventArgs) Handles btn_proxima.Click
+        tb_tentativas.Text = Val(tb_tentativas.Text) + 1
+        ListBox1.ClearSelected()
+        ListBox2.ClearSelected()
+        ListBox3.ClearSelected()
+        ListBox4.ClearSelected()
+        If TextBox1.Text Is "" Then
+            ListBox1.Enabled = True
+        End If
+        If TextBox2.Text Is "" Then
+            ListBox2.Enabled = True
+        End If
+        If TextBox3.Text Is "" Then
+            ListBox3.Enabled = True
+        End If
+        If TextBox4.Text Is "" Then
+            ListBox4.Enabled = True
+        End If
+
+
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        If TextBox1.Text IsNot "" And TextBox2.Text IsNot "" And TextBox3.Text IsNot "" AndAlso TextBox4.Text IsNot "" Then
+            MsgBox("Parabéns, ganhou!!")
+        End If
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        If TextBox1.Text IsNot "" And TextBox2.Text IsNot "" And TextBox3.Text IsNot "" AndAlso TextBox4.Text IsNot "" Then
+            MsgBox("Parabéns, ganhou!!")
+        End If
+    End Sub
+
+    Private Sub btn_sair_Click(sender As Object, e As EventArgs) Handles btn_sair.Click
+        Dim resp As Integer
+        resp = MsgBox("Tem a certeza que pretende abandonar a aplicação?", MsgBoxStyle.YesNo, "Abandonar")
+        If resp = 6 Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
